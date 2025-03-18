5ea05000d2e9eb711a592fd8c7324ee5858df442
Manav S. Rajput <theexplainer007@gmail.com>
2025-03-18 12:54:09 +0530

    Add files via upload

---
+++ functionPractice.py
@@ -0,0 +1,46 @@
+#A function is a block of reusable code that runs whenever we want to call it 
+'''
+There are three types of main functions:
+1. Built In Functions:- print(), input(), int(), float()
+2. User Defined Functions:-
+3. Lambda Functions
+
+
+'''
+
+'''
+There are 5 types of arguments also in Python:
+1. Position Argument:-
+2. Default Functions:-
+3. Keyword Functions:-
+
+'''
+def greet():
+    print("Hello World")
+    print("We are learning Python ")
+    print("Today we are learning functions in python:")
+
+def add(a,b):
+    c =a+b
+    print(c)
+
+greet()
+add(4,5)    
+
+def update(x):
+    x=7
+    print(x)
+update(7)
+
+
+
+def person(name,age):
+    print(name)
+    print(age)
+    
+person('navin', 28)
+
+'''
+Postional Argument
+
+'''
